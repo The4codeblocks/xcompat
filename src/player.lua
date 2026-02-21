b0909139d3f62012f13e7848fa375c69b8d3afca
The4codeblocks <72419529+The4codeblocks@users.noreply.github.com>
2026-02-21 06:11:00 -0500

    nevermind, precedent (again)

--- src/player.lua
+++ src/player.lua
@@ -1,6 +1,6 @@
 local filename = xcompat.gameid
 
---if we dont have a player file for the game, use fallback
+--if we dont have a player file for the game, use xcompat_agnostic
 if not xcompat.utilities.file_exists(xcompat.modpath .. "/src/player/" .. filename .. ".lua") then
     filename = "xcompat_agnostic"
 end
