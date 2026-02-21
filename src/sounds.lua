ccb2362b81edd7d50905a4eb0b87448d5f53269f
The4codeblocks <72419529+The4codeblocks@users.noreply.github.com>
2026-02-21 06:09:25 -0500

    miscopy fix

--- src/sounds.lua
+++ src/sounds.lua
@@ -1,6 +1,6 @@
 local filename = xcompat.gameid
 
---if we dont have a materials file for the game, use minetest
+--if we dont have a sounds file for the game, use fallback
 if not xcompat.utilities.file_exists(xcompat.modpath .. "/src/sounds/" .. filename .. ".lua") then
     filename = "xcompat_agnostic"
 end
