398b5380b6738f465958402ce13d59336e347b29
The4codeblocks <72419529+The4codeblocks@users.noreply.github.com>
2026-02-21 06:10:25 -0500

    xcompat_agnostic ~= minetest

--- src/player.lua
+++ src/player.lua
@@ -1,6 +1,6 @@
 local filename = xcompat.gameid
 
---if we dont have a player file for the game, use minetest
+--if we dont have a player file for the game, use fallback
 if not xcompat.utilities.file_exists(xcompat.modpath .. "/src/player/" .. filename .. ".lua") then
     filename = "xcompat_agnostic"
 end
